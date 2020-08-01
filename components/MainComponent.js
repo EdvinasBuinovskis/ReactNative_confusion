39519d8060eba8df888005fab2fdebe9a692c5af
Edvinas Buinovskis <edvinas.buinovskis@stud.vgtu.lt>
2020-08-01 19:35:05 +0300

    Navigation Part 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,33 +1,48 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishDetailComponent';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const MenuNavigator = createStackNavigator();
+
+function MenuNavigatorScreen() {
+    return(
+        <MenuNavigator.Navigator
+            initialRouteName='Menu'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#512DA8"
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                    color: "#fff"            
+                }
+            }}
+        >
+            <MenuNavigator.Screen
+                name="Menu"
+                component={Menu}
+            />
+            <MenuNavigator.Screen
+                name="Dishdetail"
+                component={Dishdetail}
+                options={{ headerTitle: "Dish Detail"}}
+            />            
+        </MenuNavigator.Navigator>
+    );
+}
 
 class Main extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
-        }
-    }
-
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
 
     render () {
         return (
-            <View>
-                <Menu dishes={this.state.dishes} 
-                    onPress={(dishId => this.onDishSelect(dishId))}
-                />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-            </View>
+            <NavigationContainer>
+                <MenuNavigatorScreen/>           
+            </NavigationContainer>
         );
     }
-
 }
 
 export default Main;
